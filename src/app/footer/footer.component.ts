99ee66c7fa184e924083e9932b41474e78bb14b8
Jhonatan <j.leo.jc98@gmail.com>
2019-03-01 22:26:35 -0400

    footer descripcion

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FooterComponent  {
+  anio: number;
+  constructor() {
+    this.anio = new Date().getFullYear();
+   }
 }
